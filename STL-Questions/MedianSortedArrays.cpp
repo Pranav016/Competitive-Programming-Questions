63ebf1c485cbc0a50a3ad2b7d31cdba8da9153c9
Pranav <pranavmendi@gmail.com>
2021-01-13 21:14:48 +0530

    Added Median of sorted arrays problem

---
+++ STL-Questions/MedianSortedArrays.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    vector<int> merge;
+    int x[1000], y[1000];
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>x[i];
+    }
+    for(int i=0;i<n;i++){
+        cin>>y[i];
+    }
+    for(int i=0;i<n;i++){
+        merge.push_back(x[i]);
+        merge.push_back(y[i]);
+    }
+    sort(merge.begin(), merge.end());
+    int mid = n-1;
+    cout<<merge[mid];
+}
+
+/*
+
+Sample input-
+5
+1 3 5 7 9
+2 4 6 8 10
+
+Sample output-
+5
+
+*/
